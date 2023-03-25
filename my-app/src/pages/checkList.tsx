9b748570746c8e65762f1837c50e21256c45825f
tjdls111 <tjdls111@naver.com>
2023-03-25 17:44:38 +0900

    feat:title 뿐 아니라 url도 추가할 수 있게 변경

--- my-app/src/pages/checkList.tsx
+++ my-app/src/pages/checkList.tsx
@@ -2,37 +2,50 @@ import React, { useState } from 'react';
 import { checkItem } from '../types/check';
 const CheckList= () => {
   const [checks, setchecks] = useState<Array<checkItem>|[]>([]);
-  const [inputValue, setInputValue] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [url, setUrl] = useState<string>('');
 
-  function handleInputChange(event:any) {
-    setInputValue(event.target.value);
+  const handleTitleChange= (event:any)=> {
+    setTitle(event.target.value);
+  }
+  const handleUrlChange= (event:any)=> {
+    setUrl(event.target.value);
   }
 
-  function handleFormSubmit(event:any) {
+  const handleFormSubmit=(event:any) =>{
     event.preventDefault();
 
     const newCheckItem = {
       id: Date.now(),
-      title: inputValue,
+      title: title,
       isChecked:false,
-      url:'https://www.google.co.kr/'
+      url:url
     };
     setchecks([...checks, newCheckItem]);
     
-    setInputValue('');
+    setTitle('');
   }
 
+  const removeItem=(id:number)=> {
+    const updatedItems = checks.filter((check:checkItem)=> check.id !==id)
+    setchecks(updatedItems);
+  }
+  
+
+  
   return (
     <div>
       <h1>check List</h1>
       <form onSubmit={handleFormSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <input type="text" value={title} onChange={handleTitleChange} />
+        <input type="text" value={url} onChange={handleUrlChange} />
         <button type="submit">Add check</button>
       </form>
       <ul>
         {checks &&checks.map((check:checkItem) => (
           <li key={check.id}>{check.title}
             <a href={check.url} target="_blank" rel="noreferrer">Go to site</a>
+            <button onClick={() => removeItem(check.id)}>Remove</button>
           </li>
         ))}
       </ul>
