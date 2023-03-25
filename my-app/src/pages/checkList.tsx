023a515112c44c2a113a7ec6f6393023ccb06ad5
tjdls111 <tjdls111@naver.com>
2023-03-25 17:23:33 +0900

    feat: check list add, list 기능

--- my-app/src/pages/checkList.tsx
+++ my-app/src/pages/checkList.tsx
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
+import { checkItem } from '../types/check';
+const CheckList= () => {
+  const [checks, setchecks] = useState<Array<checkItem>|null>(null);
+  const [inputValue, setInputValue] = useState<string>('');
 
-function CheckList() {
-  const [checks, setchecks] = useState([
-    { id: 1, text: 'Buy groceries' },
-    { id: 2, text: 'Do laundry' },
-    { id: 3, text: 'Clean the house' }
-  ]);
+  function handleInputChange(event:any) {
+    setInputValue(event.target.value);
+  }
+
+  function handleFormSubmit(event:any) {
+    event.preventDefault();
+
+    const newCheckItem = {
+      id: Date.now(),
+      title: inputValue,
+      isChecked:false,
+      url:'dummy url'
+    };
+    if (checks){
+      setchecks([...checks, newCheckItem]);
+    }
+    else{
+      setchecks([ newCheckItem]);
+    }
+    
+    setInputValue('');
+  }
 
   return (
     <div>
       <h1>check List</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Add check</button>
+      </form>
       <ul>
-        {checks.map(check => (
-          <li key={check.id}>{check.text}</li>
+        {checks &&checks.map((check:checkItem) => (
+          <li key={check.id}>{check.title}</li>
         ))}
       </ul>
     </div>
