8da826e1c98ea864d298bc7e2a51b10d1f820ee4
Seoin <68271159+tjdls111@users.noreply.github.com>
2023-03-28 21:33:40 +0900

    chore:Add Eslint, Prettier (#6)

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 import {
   createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import CheckList from './pages/checkList';
+  RouterProvider
+} from 'react-router-dom'
+import CheckList from './pages/checkList'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <CheckList/>,
-  },
-]);
+    path: '/',
+    element: <CheckList/>
+  }
+])
 
 root.render(
   <React.StrictMode>
        <RouterProvider router={router} />
     <App />
   </React.StrictMode>
-);
+)
--- my-app/src/pages/checkList.tsx
+++ my-app/src/pages/checkList.tsx
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
-import CheckListForm from '../components/CheckListForm';
-import { checkItem } from '../types/check';
-const CheckList= () => {
-  const [checks, setChecks] = useState<Array<checkItem>|null>(null);
+import React, { useState } from 'react'
+import CheckListForm from '../components/CheckListForm'
+import { type checkItem } from '../types/check'
+const CheckList = () => {
+  const [checks, setChecks] = useState<checkItem[] | null>(null)
 
+  const removeItem = (id: number) => {
+    if (checks == null) return
 
-  const removeItem=(id:number)=> {
-    if (!checks)return
-    
-
-    const updatedItems = checks.filter((check:checkItem)=> check.id !==id)
-    setChecks(updatedItems);
+    const updatedItems = checks.filter((check: checkItem) => check.id !== id)
+    setChecks(updatedItems)
   }
-  
 
-  
   return (
     <div>
       <h1>check List</h1>
       <CheckListForm checks={checks} setChecks={setChecks}/>
       <ul>
-        {checks &&checks.map((check:checkItem) => (
+        {(checks != null) && checks.map((check: checkItem) => (
           <li key={check.id}>{check.title}
             <a href={check.url} target="_blank" rel="noreferrer">Go to site</a>
-            <button onClick={() => removeItem(check.id)}>Remove</button>
+            <button onClick={() => { removeItem(check.id) }}>Remove</button>
           </li>
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default CheckList;
+export default CheckList
